1234cc5ba5dd114fe36ecd7ddbdc6a0fd81afd15
Evan Greenstein <evanlg16@gmail.com>
2024-01-20 13:18:10 -0500

    Added more libs

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from math import e
+from flask import Flask, jsonify, request, make_response
+from flask_cors import CORS
+from dotenv import load_dotenv
+import requests, os, json
+from functools import wraps
+import pandas as pd
+import numpy as np
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
-@app.route('/')
-def hello():
+@app.route('/api/get-statistics', methods=['GET'])
+def get_statistics():
     return 'Hello, World!'
+
+# Start the server
+if __name__ == '__main__':
+    app.run(port=4000, debug=True)
