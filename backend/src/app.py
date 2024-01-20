17da3422f30d5ecb33bfa6ef0b12f175fa357782
pop-cm <popcm@live.ca>
2024-01-20 14:55:13 -0500

    Extract and send back relevant crime rows

--- backend/src/app.py
+++ backend/src/app.py
@@ -29,9 +29,13 @@ def get_statistics():
         crime_df = pd.read_csv( os.path.join(os.path.dirname(__file__), "../datasets/actes-criminels.csv") )
         print("3")
 
-        return make_response(jsonify(crime_df.to_dict()), 200)
+        # Filter out rows with NaN values in longitude or latitude
+        crime_df = crime_df.dropna(subset=['LONGITUDE', 'LATITUDE'])
+        crime_array = crime_df[['CATEGORIE', 'LONGITUDE', 'LATITUDE']].values.tolist()
+
+        return make_response(jsonify(crime_array), 200)
     except Exception as e :
-        return make_response(jsonify({"error": e}), 500)
+        return make_response(jsonify({"error": str(e)}), 500)
 
 
 # Start the server
