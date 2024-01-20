47f03cab2937265971e871b619e6fec3959fd328
Evan Greenstein <evanlg16@gmail.com>
2024-01-20 14:08:24 -0500

    Update app.py

--- backend/src/app.py
+++ backend/src/app.py
@@ -14,13 +14,22 @@ CORS(app)  # Enable CORS for all routes
 
 @app.route('/api/get-statistics', methods=['POST'])
 def get_statistics():
-    content_type = request.headers.get('Content-Type')
-    if (content_type != 'application/json'):
-        return make_response(jsonify({"error": "Content type not application/json"}), 500)
-    
-    request_body = request.json
+    try:
 
-    return make_response("WIP", 200)
+        content_type = request.headers.get('Content-Type')
+        if (content_type != 'application/json'):
+            return make_response(jsonify({"error": "Content type not application/json"}), 500)
+        
+        # Will use later
+        request_body = request.json
+
+        data_folder = '../datasets'
+        crime_df = pd.read_csv(data_folder+ "/actes-criminels.csv")
+        
+        return make_response(crime_df.to_dict(), 200)
+    except Exception as e :
+        return make_response(jsonify({"error": e}), 500)
+
 
 # Start the server
 if __name__ == '__main__':
