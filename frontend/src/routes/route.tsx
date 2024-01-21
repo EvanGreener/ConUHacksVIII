80bc41b8a7dd54b7f82666a82d4daadf6e8029c7
pop-cm <popcm@live.ca>
2024-01-21 10:53:20 -0500

    Add post form

--- frontend/src/routes/route.tsx
+++ frontend/src/routes/route.tsx
@@ -1,6 +1,7 @@
 import TrendingPosts from "../components/TrendingPosts";
 import PostDetailed from "../components/PostDetailed";
 import Navbar from "../components/Navbar.tsx";
+import PostForm from "../components/PostForm.tsx";
 
 export default function Root() {
   return (
@@ -15,6 +16,9 @@ export default function Root() {
         </div>
         <div>
           <PostDetailed/>
+        </div>
+        <div>
+          <PostForm/>
         </div>
       </div>
     </>
