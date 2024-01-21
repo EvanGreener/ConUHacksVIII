4218861a112c96c7d23a25cf6a0cef0d13fc0d5b
Evan Greenstein <evanlg16@gmail.com>
2024-01-20 20:27:19 -0500

    Made route for sign up and creating posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,7 +1,41 @@
 const express = require("express");
 var router = express.Router();
+const auth = require("../middleware/auth");
+const { getFirestore, Timestamp, FieldValue } = require("firebase-admin/firestore");
+const db = getFirestore();
 
-router.get("/", function (req, res) {
+router.post("/create-post/:uid", auth, async (req, res) => {
+  try {
+    const uid = req.params.uid;
+    const { firstName, lastName, education, occupation } = req.body;
+
+    const postData = {
+      firstName,
+      lastName,
+      education,
+      occupation,
+      createdTS: FieldValue.serverTimestamp(),
+      lastModifiedTS: FieldValue.serverTimestamp(),
+      comments: [],
+    };
+
+    const postRef = await db.collection("posts").add(postData);
+    const postId = postRef.id;
+
+    await db
+      .collection("users")
+      .doc(uid)
+      .update({ posts: FieldValue.arrayUnion(postId)});
+
+    res.send("");
+  } catch (error) {
+    console.error("Error creating post", error);
+    res.status(500).send(error.message);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
   res.send("");
 });
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,18 +5,18 @@ const { getFirestore } = require("firebase-admin/firestore");
 const db = getFirestore();
 
 // Route to create a user
-router.post("/sign-up", auth, async (req, res) => {
-  const { uid, firstName, lastName, education, occupation } = req.body;
-
-  // Check if user already exists
-  const doc = await db.collection("users").doc(uid).get();
-  // Return 210 because when signing in with Google, the user is already created
-  // TODO: add an independent sign up route
-  if (doc.exists) {
-    return res.status(210).send("User already exists");
-  }
-
+router.post("/sign-up-regular", auth, async (req, res) => {
   try {
+    const { uid, firstName, lastName, education, occupation } = req.body;
+
+    // Check if user already exists
+    const doc = await db.collection("users").doc(uid).get();
+    // Return 210 because when signing in with Google, the user is already created
+    // TODO: add an independent sign up route
+    if (doc.exists) {
+      return res.status(210).send("User already exists");
+    }
+
     await db.collection("users").doc(uid).set({
       firstName,
       lastName,
@@ -30,4 +30,28 @@ router.post("/sign-up", auth, async (req, res) => {
   }
 });
 
+router.post("/sign-up-researcher", auth, async (req, res) => {
+  try {
+    const { uid, firstName, lastName, education, occupation } = req.body;
+
+    // Check if user already exists
+    const doc = await db.collection("users").doc(uid).get();
+    // Return 210 because when signing in with Google, the user is already created
+    // TODO: add an independent sign up route
+    if (doc.exists) {
+      return res.status(210).send("User already exists");
+    }
+
+    await db.collection("users").doc(uid).set({
+      firstName,
+      lastName,
+      education,
+      occupation,
+    });
+    return res.status(201).send("User created successfully");
+  } catch (error) {
+    console.error("Error creating user", error);
+    res.status(500).send(error.message);
+  }
+});
 module.exports = router;
